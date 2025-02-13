d90a6a1ccc4016755786321c2eadbda0dccb6131
Vinicius Lopes <vinicius.lopes.230996@gmail.com>
2025-02-13 09:08:09 -0300

    feat: implement QuestionBestAnswerChosenEvent and notification handler; trigger notifications on best answer selection

--- src/domain/forum/enterprise/entities/question.ts
+++ src/domain/forum/enterprise/entities/question.ts
@@ -4,6 +4,7 @@ import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { Slug } from './values-objects/slug'
 import { AggregateRoot } from '@/core/entities/aggregate-root'
 import { QuestionAttachmentList } from './question-attachment-list'
+import { QuestionBestAnswerChosenEvent } from '../events/question-best-answer-chosen-event'
 
 export interface QuestionProps {
   bestAnswerId?: UniqueEntityID
@@ -69,6 +70,14 @@ export class Question extends AggregateRoot<QuestionProps> {
   }
 
   set bestAnswerId(bestAnswerId: UniqueEntityID | undefined) {
+    if (bestAnswerId === undefined) return
+
+    if (
+      this.props.bestAnswerId === undefined ||
+      !this.props.bestAnswerId.equals(bestAnswerId)
+    )
+      this.addDomainEvent(new QuestionBestAnswerChosenEvent(this, bestAnswerId))
+
     this.props.bestAnswerId = bestAnswerId
     this.touch()
   }
